4926d04e0d6ca852097e4cc4e74c9fac661c14f7
Rohit Jadhav <vishubn305@gmail.com>
2026-03-31 07:28:07 +0530

    Intial commit

--- script.sh
+++ script.sh
@@ -1,8 +1,3 @@
-echo "jenking job started"
-echo "user: $(whomami)"
-echo "date: $(date)"
-echo "current directory: $(pwd)"
-echo "listing files:"
-ls -l
-echo "Job completed successfully"
+echo "Hello From Jenkins"
+date
 
